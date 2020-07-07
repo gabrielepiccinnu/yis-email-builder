ab540970e49f556d865cd8d4a4b07ed9c5542569
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2020-07-07 16:11:32 +0200

    Test OK 16:11

--- src/Emailbuilder.js
+++ src/Emailbuilder.js
@@ -1,66 +1,60 @@
 import React, { useRef, Component } from 'react';
-import { render } from 'react-dom'
+//import { render } from 'react-dom'
 
 import EmailEditor from 'react-email-editor';
 
-const App = (props) => {
-  const emailEditorRef = useRef(null);
-
-  const exportHtml = () => {
-    emailEditorRef.current.editor.exportHtml((data) => {
-      const { design, html } = data;
-      console.log('exportHtml', html);
-    });
-  };
-
-  const onLoad = () => {
-    // you can load your template here;
-    // const templateJson = {};
-    // emailEditorRef.current.editor.loadDesign(templateJson);
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={exportHtml}>Export HTML</button>
-      </div>
-      
-      <EmailEditor
-        ref={emailEditorRef}
-        onLoad={onLoad}
-      />
-    </div>
-  );
-};
-
 class Emailbuilder extends Component {
+ 
 
-
-    
     exportHtml = () => {
-     /*   emailEditorRef.current.editor.exportHtml((data) => {
-          const { design, html } = data;
-          console.log('exportHtml', html);
-        });*/
-      };
-    
-      onLoad = () => {
+        /*   emailEditorRef.current.editor.exportHtml((data) => {
+             const { design, html } = data;
+             console.log('exportHtml', html);
+           });*/
+    };
+
+    onLoad = () => {
         // you can load your template here;
         // const templateJson = {};
         // emailEditorRef.current.editor.loadDesign(templateJson);
-      };
+    };
 
     render() {
         return (
             <div>
-              <h2>Email Builder</h2>
+                <h2>Email Builder</h2>
+                <button onClick={this.saveDesign}>Save Design</button>
+                <button onClick={this.exportHtml}>Export HTML</button>
 
-              <EmailEditor
-        //ref={emailEditorRef}
-        onLoad={this.onLoad}
-      />
+                <EmailEditor
+                    minHeight='800px'    
+                    locale='it'
+                    ref={editor => this.editor = editor}
+                    onLoad={this.onLoad}
+                />
             </div>
         );
-      }
     }
-    export default Emailbuilder;
+    saveDesign = () => {
+        this.editor.saveDesign(design => {
+            console.log('saveDesign', design);
+            alert('Il Design JSON è in console.');
+
+        })
+    }
+
+    exportHtml = () => {
+        this.editor.exportHtml(data => {
+            const { design, html } = data
+            console.log('exportHtml', html);
+            alert('L\'output HTML è stato copiato negli appunti.');
+
+        })
+    }
+
+    onLoad = () => {
+        //const json = 'design'
+         //   this.editor.loadDesign(json)
+    }
+}
+export default Emailbuilder;
