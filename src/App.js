ab04022051f78ff857456fb6a6622b0dceac186c
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2020-10-07 13:03:55 +0200

    nuovo template 07 10 2020

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Emailbuilderit from './Emailbuilder-it';
 import Emailbuilderit11092020 from './Emailbuilder-it-11092020';
 import Emailbuilderit25092020 from './Emailbuilder-it-25092020';
 import Emailbuilderit02102020 from './Emailbuilder-it-02102020';
+import Emailbuilderit07102020 from './Emailbuilder-it-07102020';
 import {Layout} from "./components/Layout";
 
 
@@ -36,6 +37,7 @@ class App extends Component {
               <Route path='/emailbuilder-template-ita-11092020' component={Emailbuilderit11092020} />
               <Route path='/emailbuilder-template-ita-25092020' component={Emailbuilderit25092020} />
               <Route path='/emailbuilder-template-ita-02102020' component={Emailbuilderit02102020} />
+              <Route path='/emailbuilder-template-ita-07102020' component={Emailbuilderit07102020} />
               <Route path='/contact' component={Contact} />
               <Route path='/about' component={About} />
               <Route path='/testmenu' component={TestMenu} />
